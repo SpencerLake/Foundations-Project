28a3fff5ba7f03c6cb25dd9b481a8f80f4796c6b
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-06-19 11:28:22 -0400

    Redoing old project layout

---
+++ client/home.js
@@ -0,0 +1,59 @@
+// Defining Variables ===========================================================================
+const newFeed = document.querySelector('#div-display')
+const climbList = document.querySelector('#clmb-list')
+const projectIdea = document.querySelector('#add')
+const baseURL = `/api/posts`
+const displayArea = document.getElementsByClassName("post-template")
+
+const postsCallback = ({ data: posts}) => displayPosts(posts)
+const errCallback = err => console.log(err)
+
+const getAllPosts = () => axios.get(baseURL)
+        .then(postsCallback).catch(errCallback)
+const deletePost = id => axios.delete(`${baseURL}/${id}`)
+        .then(cleanPosts)
+        .then(postsCallback).catch(errCallback)
+
+
+
+// Calling and creating template for each post ==================================================
+
+// Function that defines the template of each post
+function creatingPost(post) {
+    const postTemplate = document.createElement('div')
+    postTemplate.classList.add('post-template')
+
+    postTemplate.innerHTML = `<div class="post" id="${post.title}">
+    <div id="post-title">${post.title}</div>
+    <div id="post-contents">
+        <div id="special-notice">
+                <p>${post.grade}</p>
+                <p>${post.time}</p>
+        </div>
+        <div id="images">
+            <img class="pictures" src="${post.pic}"/>
+        </div>
+        <div id="empty">
+            <button class="delete-button" onclick="deletePost(${post.id})">X</button>
+        </div>
+    </div>
+    </div>
+    `
+
+    newFeed.appendChild(postTemplate)
+}
+
+// Function that calls the array of posts and creates each one to be
+// displayed on front page.
+function displayPosts(arr) {
+    arr.forEach((post) => {
+        creatingPost(post)
+    })
+}
+
+function cleanPosts() {
+    displayArea.remove()
+}
+
+// Calls the function to populate page with posts
+getAllPosts()
--- client/script.js
+++ client/script.js
@@ -1,20 +1,21 @@
-
-
+// Setting Variables ===========================================================
 const newFeed = document.querySelector('#div-display')
 const climbList = document.querySelector('#clmb-list')
 const projectIdea = document.querySelector('#add')
 
 const baseURL = `/api/posts`
 
-const postsCallback = ({ data: posts}) => displayPosts(posts)
+const ideaCallback = ({ data: ideas }) => displayIdea(ideas)
+
 const errCallback = err => console.log(err)
 
-const getAllPosts = () => axios.get(baseURL).then(postsCallback).catch(errCallback)
-const createPost = body => axios.post(baseURL, body).then(postsCallback).catch(errCallback)
+const getRandomIdea = () => axios.get(baseURL).then(ideaCallback).catch(errCallback)
+const createPost = body => axios.post(baseURL, body).catch(errCallback)
 const deletePost = id => axios.delete(`${baseURL}/${id}`).then(postsCallback).catch(errCallback)
 
     
 
+// Post Submissions ===========================================================
 function submitHandler(e) {
     e.preventDefault()
 
@@ -22,88 +23,45 @@ function submitHandler(e) {
     let time = document.querySelector('#tms')
     let name = document.querySelector('#title-add')
     let pic = document.querySelector('#photo-add')
-
+    
     let bodyObj = {
         title: name.value,
         grade: grade.value,
         time: time.value,
         pic: pic.value
     }
-
+    
     createPost(bodyObj)
-
+    
     name.value = ''
     grade.value = ''
     time.value = ''
     pic.value = ''
-
+    
     console.log('submit handled')
 }
 
-function creatingPost(post) {
-    const postTemplate = document.createElement('div')
-    postTemplate.classList.add('post-template')
+// Code that defines the form and listens for when it is submitted =============
+const addForm = document.getElementById("add");
+addForm.addEventListener('submit', submitHandler);
 
-    postTemplate.innerHTML = `<div class="post" id="${post.title}">
-    <div id="post-title">${post.title}</div>
-    <div id="post-contents">
-        <div id="special-notice">
-            <ul>
-                <li>${post.grade}</li>
-                <li>${post.time}</li>
-            </ul>
-        </div>
-        <div id="images">
-            <img class="pictures" src="${post.pic}"/>
-        </div>
-        <div id="empty">
-            <button class="delete-button" onclick="deletePost(${post.id})">X</button>
-        </div>
-    </div>
-    </div>
-    `
 
-    newFeed.appendChild(postTemplate)
 
-    console.log('post created')
+// Code for ideas button =======================================================
 
-    const newList = document.createElement('div')
-    newList.classList.add('list-item')
-
-    newList.innerHTML = `
-        <div class="list-item" href="#${post.title}">
-            <div class="list-icon"></div>
-            <h3 id="${post.title}-list">${post.title}</h4>
-    `
-    
-    climbList.appendChild(newList)
-}
-
-function displayPosts(arr) {
-    newFeed.innerHTML = ``
-    climbList.innerHTML = ``
-    for (let i = 0; i < arr.length; i++) {
-        creatingPost(arr[i])
-    }
-}
-
+// List of ideas
 let ideas = [
-    "Flying Dutchman",
-    "Pineapple Epxress",
-    "Bat Hang",
-    "Behemoth",
-    "Pinchy Hang",
-    "Infinidy Toes",
-    "Slippery Sloper",
-    "Dank Dyno",
-    "Flyin Squirell",
-    "Crimp Ladder"
+    "Collapse",
+    "Vindictive"
 ]
-
+// Randomizes which idea will be prompted
 const randomIdea = ideas[Math.floor(Math.random()*ideas.length)]
 
+// Listener for when get idea button is clicked
+const projIdea = document.getElementById('proj-idea')
+projIdea.addEventListener('click', getIdea)
 
-
+// Function that displays an idea
 function getIdea(e) {
     e.preventDefault()
     
@@ -114,19 +72,3 @@ function getIdea(e) {
 
     console.log()
 }
-
-
-const addForm = document.getElementById("add");
-addForm.addEventListener('submit', submitHandler);
-
-const exList = document.getElementById('flying-dutchman')
-function scroll() {document.getElementById('example').scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});}
-
-const listSnap = document.getElementById
-
-
-
-const projIdea = document.getElementById('proj-idea')
-projIdea.addEventListener('click', getIdea)
-
-getAllPosts()
